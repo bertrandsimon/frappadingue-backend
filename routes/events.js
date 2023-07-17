bc46716a8a0963865badc5998f41095147f5bb43
bertrandsimon <trandber@hotmail.com>
2023-07-17 15:52:22 +0200

    DELETE single event route created

--- routes/events.js
+++ routes/events.js
@@ -13,16 +13,33 @@ router.get("/allEvents", (req, res) => {
 });
 
 //http://localhost:3000/events/single_event/:id
+// http://localhost:3000/events/single_event/64b500f34b1ba69f4fe2612d
 //get single event for selected event
-// ie : 64b500f34b1ba69f4fe2612b
 
-// http://localhost:3000/events/single_event/64b500f34b1ba69f4fe2612b
 
 router.get("/single_event/:id", (req, res) => {
   Event.findById(req.params.id).then((data) =>
     res.json({ result: true, event: data })
   );
 });
+
+
+//http://localhost:3000/events/delete/:id
+//delete jobs with id
+// http://localhost:3000/events/delete/64b500f34b1ba69f4fe2612d
+router.delete("/delete/:id", (req, res) => {
+  Event.deleteOne({
+    _id: req.params.id,
+  }).then((data) => {
+    if (data.deletedCount > 0) {
+      console.log(data);
+      res.json({ result: true, data: data });
+    } else {
+      res.json({ result: false, error: "event not found" });
+    }
+  });
+});
+
 
 
 module.exports = router;
